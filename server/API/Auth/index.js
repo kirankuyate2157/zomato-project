f2c1217fd1bfeb3c73530113fa1067b0b66f6555
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-08-20 16:15:30 +0530

    add statics and method refactor signup route

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,7 +1,7 @@
 //Library
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
 
 // Models
 import { UserModel } from "../../database/user";
@@ -18,26 +18,13 @@ Route       /Signup
 
 Router.post("/signup", async (req, res) => {
   try {
-    const { email, password, fullName, phoneNumber } = req.body.credentials;
-
-    //check whether email is exist
-    const checkUserByEmail = await UserModel.findOne({ email });
-    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-
-    if (checkUserByEmail || checkUserByPhone) {
-      return res.json({ error: "User already exists!" });
-    }
-    //has password
-    const bcryptSalt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+    await UserModel.findByEmailAndPhoneNumber(req.body.credentials);
 
     //save to DB
-    await UserModel.create({
-      ...req.body.credentials,
-      password: hashedPassword,
-    });
+    const newUser = await UserModel.create(req.body.credentials);
+
     //generate JWT auth token
-    const token = jwt.sign({ user: { fullName, email } }, "zomatoAPP");
+    const token = newUser.generateJwtToken();
 
     //return
     return res.status(200).json({ token, status: "success" });
--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,4 +1,6 @@
-import Mongoose from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -13,4 +15,41 @@ const UserSchema = new mongoose.Schema(
   }
 );
 
+UserSchema.methods.generateJwtToken = function () {
+  return jwt.sign({ user: this._id.toString() }, "zomatoAPP");
+};
+
+UserSchema.statics.findByEmailAndPhoneNumber = async ({
+  email,
+  phoneNumber,
+}) => {
+  //check whether email is exist
+  const checkUserByEmail = await UserModel.findOne({ email });
+  const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+
+  if (checkUserByEmail || checkUserByPhone) {
+    throw new Error("User already Exists...!");
+  }
+  return false;
+};
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+  //password is  modified
+  if (!user.isModified("password")) return next();
+
+  //generate bcrypt Salt
+  bcrypt.genSalt(8, (error, salt) => {
+    if (error) return next(error);
+
+    //hash the password
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+
+      //assigning  hashed password
+      user.password = hash;
+      return next();
+    });
+  });
+});
 export const UserModel = mongoose.model("Users", UserSchema);
