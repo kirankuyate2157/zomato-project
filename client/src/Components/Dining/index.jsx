8584bbbfaca57582d29429a1fec2eaf1534b8717
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-08-29 20:27:14 +0530

    dinining out done

--- client/src/Components/Dining/index.jsx
+++ client/src/Components/Dining/index.jsx
@@ -5,9 +5,16 @@ import DiningCarousal from "./DiningCarousal";
 
 const Dining = () => {
   return (
-    <div className=" md:px-3 lg:container lg:px-20 mx-auto">
-      <DiningCarousal />
-    </div>
+    <>
+      <div className="bg-gray-100 h-full pb-7">
+        <DiningCarousal />
+      </div>
+      <div className=" container mx-auto px-4 lg:px-20 ">
+        <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+          Best Dining Restaurants near you in Koregaon Park
+        </h1>
+      </div>
+    </>
   );
 };
 
