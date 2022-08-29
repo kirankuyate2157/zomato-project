839b4a01ec775a824c79bb0b35d3995b3611f188
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-08-29 19:23:28 +0530

    dining caurousal done 😎

---
+++ client/src/Components/Dining/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//components
+import DiningCarousal from "./DiningCarousal";
+
+const Dining = () => {
+  return (
+    <div className=" md:px-3 lg:container lg:px-20 mx-auto">
+      <DiningCarousal />
+    </div>
+  );
+};
+
+export default Dining;
--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 import { useParams } from "react-router-dom";
+// components
 import Delivery from "./Delivery";
+import Dining from "./Dining";
 
 const Master = () => {
   const { type } = useParams();
-  return <div className="my-5">{type === "delivery" && <Delivery />}</div>;
+  return (
+    <div className="my-5">
+      {type === "delivery" && <Delivery />}
+      {type === "dining" && <Dining />}
+    </div>
+  );
 };
 
 export default Master;
