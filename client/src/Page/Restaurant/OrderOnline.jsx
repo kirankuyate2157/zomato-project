5190de6f985031e9576290fddd795227a2cfecf8
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-09-04 18:44:19 +0530

    food cards added in online food

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 
+import { AiOutlineCompass } from "react-icons/ai";
+import { BiTimeFive } from "react-icons/bi";
+
 // components
 import FloatMenuBtn from "../../Components/Order-Online/FloatMenuBtn";
+import FoodItem from "../../Components/Order-Online/FoodItem";
+import MenuListContainer from "../../Components/Order-Online/MenuListContainer";
 
 const OrderOnline = () => {
     return (
         <>
-            <div className="w-full">SD</div>
+            <div className="w-full flex">
+                <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
+                    <MenuListContainer />
+                    <MenuListContainer />
+                </aside>
+                <div className="w-full md:w-3/4">
+                    <h2 className="text-xl font-semibold">Order Online</h2>
+                    <h4 className="flex items-center gap-2 font-light text-gray-500">
+                        <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+                    </h4>
+                    <section>
+                        <FoodItem
+                            image="https://b.zmtcdn.com/data/dish_photos/839/62575c08ce26635e3f48b3642805d839.jpg?output-format=webp"
+                            price="1000"
+                            rating={3}
+                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dicta repellendus sit cumque eum odio animi doloribus quo voluptatem aut ipsam, consectetur non minima rerum eligendi magnam aspernatur culpa provident?"
+                            title="Yummy food"
+                        />
+                    </section>
+                </div>
+            </div>
             <FloatMenuBtn />
         </>
     );
