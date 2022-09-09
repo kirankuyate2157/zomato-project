244c1823162d208b8cf37d2c53adc705fa9cf272
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-09-09 13:13:58 +0530

    cart reducer added 🧐

--- client/src/Redux/Reducer/rootReducer.js
+++ client/src/Redux/Reducer/rootReducer.js
@@ -2,6 +2,18 @@ import { combineReducers } from "redux";
 
 import restaurant from "./restaurant/restaurant.reducer";
 import image from "./Image/Image.reducer";
+import reviews from "./Reviews/review.reducer";
+import user from "./User/user.reducer";
+import food from "./Food/Food.reducer";
+import cart from "./Cart/Cart.reducer";
 
-const rootReducer = combineReducers({ restaurant, image });
+const rootReducer = combineReducers({
+  restaurant,
+  image,
+  reviews,
+  user,
+  food,
+  cart,
+});
+
 export default rootReducer;
