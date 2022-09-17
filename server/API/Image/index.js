e3322c26296e5e4b2d425cafd768b42b5cea19d7
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-09-17 20:51:24 +0530

    images updated in DB ✳🌟

--- server/API/Image/index.js
+++ server/API/Image/index.js
@@ -15,6 +15,23 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 /*
+Route     /
+Des       Get Image details
+Params    _id
+Access    Public
+Method    GET  
+*/
+Router.get("/:_id", async (req, res) => {
+  try {
+    const image = await ImageModel.findById(req.params._id);
+
+    return res.json({ image });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/*
 Route       /
  Des        Uploads given image to S3 bucket,and saves file link to mongodb
  Params     none
@@ -36,6 +53,8 @@ Router.post("/", upload.single("file"), async (req, res) => {
     };
 
     const uploadImage = await s3Upload(bucketOptions);
+
+    await ImageModel.create({ images: [{ location: uploadImage.Location }] });
 
     return res.status(200).json({ uploadImage });
   } catch (error) {
