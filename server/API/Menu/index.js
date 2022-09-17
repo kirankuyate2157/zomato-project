4ae201a03203f44d186a3606acc5eee052e94290
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-09-17 20:48:07 +0530

    menu api updated 🤤

--- server/API/Menu/index.js
+++ server/API/Menu/index.js
@@ -50,4 +50,94 @@ Router.get("/image/:_id", async (req, res) => {
   }
 });
 
+/*
+Route       /new
+ Des        add new menu to database
+ Params     none
+ Access     public
+ Method     POST
+*/
+
+Router.post("/new", async (req, res) => {
+  try {
+    const { menuData } = req.body;
+
+    if (menuData._id) {
+      const updateMenu = await MenuModel.findByIdAndUpdate(
+        menuData._id,
+        {
+          $push: {
+            menus: { $each: menuData.menus },
+          },
+        },
+        { new: true }
+      );
+
+      return res.json({ menu: updateMenu });
+    }
+
+    const createNewMenu = await MenuModel.create(menuData);
+
+    return res.json({ menu: createNewMenu });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/*
+Route       /recommendation/new
+ Des        add new recommendation
+ Params     none
+ Access     public
+ Method     POST
+*/
+
+Router.post("/recommendation/new", async (req, res) => {
+  try {
+    const { menuData } = req.body;
+
+    const updateMenu = await MenuModel.findByIdAndUpdate(
+      menuData._id,
+      {
+        $push: {
+          recommended: { $each: menuData.recommended },
+        },
+      },
+      { new: true }
+    );
+
+    return res.json({ menu: updateMenu });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/*
+Route       /update
+ Des        update new menu database
+ Params     none
+ Access     public
+ Method     PATCH
+*/
+
+Router.patch("/update", async (req, res) => {
+  try {
+    const { menuData } = req.body;
+    const updateMenu = await MenuModel.findOneAndUpdate(
+      { _id: menuData._id, "menus._id": menuData.menu_id },
+      {
+        $set: {
+          "menus.$.name": menuData.name,
+        },
+        $push: { "menus.$.items": { $each: menuData.items } },
+      },
+      { new: true }
+    );
+
+    return res.json({ menu: updateMenu });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 export default Router;
